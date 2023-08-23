f13fd12bb12cc613c8e39bd47a2d1b21c2408c9c
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2023-08-23 06:22:06 +0000

    removed redis_manager

--- redis_manager/redis_manager.py
+++
@@ -1,9 +0,0 @@
-import redis
-import json
-
-redis_client = redis.Redis(host='redis-container', port=6379, db=0)  # Use the container name of Redis
-
-def update_redis(payload):
-    sensor_id = payload["sensor_id"]
-    redis_client.lpush(sensor_id, json.dumps(payload))
-    redis_client.ltrim(sensor_id, 0, 9)
