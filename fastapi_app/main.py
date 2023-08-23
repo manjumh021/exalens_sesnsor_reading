e93fad7bb2d9cb240068442fc16c95ed0423418e
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2023-08-23 11:43:00 +0530

    modification in subscriber and fast API fetching

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -2,36 +2,72 @@ from fastapi import FastAPI, HTTPException
 import pymongo
 import redis
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = FastAPI()
 
 @app.get("/sensor_readings")
-def get_sensor_readings(start: str, end: str):
-    client = pymongo.MongoClient("mongodb://mongodb-container:27017/")  # Use the container name of MongoDB
-    db = client["sensor_db"]
-    collection = db["sensor_readings"]
-    
-    start_time = parse_datetime(start)
-    end_time = parse_datetime(end)
-    
-    readings = collection.find({
-        "timestamp": {
-            "$gte": start_time,
-            "$lte": end_time
-        }
-    })
-    
-    return [reading for reading in readings]
+def get_sensor_readings(topic: str, start: str, end: str):
+    if topic == "sensors_temperature" or topic == "sensors_humidity":
+        client = pymongo.MongoClient("mongodb://mongodb-container:27017/")
+        db = client["sensor_db"]
+        collection = db[topic]
+        
+        start_time = convert_to_custom_format(start)
+        end_time = convert_to_custom_format(end)
+        
+        readings = collection.find({
+            "timestamp": {
+                "$gte": start_time,
+                "$lte": end_time
+            }
+        })
+        
+        # Convert ObjectId to string for JSON serialization
+        readings_list = []
+        for reading in readings:
+            reading_dict = dict(reading)
+            reading_dict["_id"] = str(reading["_id"])
+            readings_list.append(reading_dict)
+        
+        return readings_list
+    else:
+        raise HTTPException(status_code=400, detail="Invalid topic chosen.")
+
 
 @app.get("/last_ten_readings")
-def get_last_ten_readings(sensor_id: str):
-    redis_client = redis.Redis(host='redis-container', port=6379, db=0)  # Use the container name of Redis
-    readings = redis_client.lrange(sensor_id, 0, -1)
-    return [json.loads(reading) for reading in readings]
+def get_last_ten_readings(topic: str, sensor_id: str):
+    if topic == "sensors_temperature" or topic == "sensors_humidity":
+        redis_key = f"{topic}:{sensor_id}"  # Create the Redis key for the topic and sensor ID
+        redis_client = redis.Redis(host='redis-container', port=6379, db=0)
+        readings = redis_client.lrange(redis_key, 0, -1)
+        return [json.loads(reading) for reading in readings]
+    else:
+        raise HTTPException(status_code=400, detail="Invalid topic for last_ten_readings")
 
-def parse_datetime(datetime_str):
+def convert_to_custom_format(datetime_str):
     try:
-        return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%SZ")
-    except ValueError:
-        raise ValueError("Invalid datetime format. Expected ISO8601 format: 'YYYY-MM-DDTHH:MM:SSZ'")
+        input_formats = [
+            "%Y-%m-%d %H:%M:%S%z",
+            "%Y-%m-%d",
+            "%Y-%m-%dT%H:%M:%S",
+            "%Y-%m-%dT%H:%M:%S.%f",
+            "%Y-%m-%dT%H:%M:%SZ"
+        ]
+        
+        dt = None
+        for format_str in input_formats:
+            try:
+                dt = datetime.strptime(datetime_str, format_str)
+                break
+            except ValueError:
+                pass
+        
+        if dt is None:
+            raise ValueError("Invalid datetime format")
+        
+        formatted_dt = dt.replace(tzinfo=timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+        
+        return formatted_dt
+    except ValueError as e:
+        return str(e)
--- mqtt_subscriber/mqtt_subscriber.py
+++ mqtt_subscriber/mqtt_subscriber.py
@@ -9,21 +9,25 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("sensors/temperature")
     client.subscribe("sensors/humidity")  # Subscribe to the humidity topic as well
 
-def update_redis(payload):
+def update_redis(payload, topic):
     sensor_id = payload["sensor_id"]
-    redis_client.lpush(sensor_id, json.dumps(payload))
-    redis_client.ltrim(sensor_id, 0, 9)
+    redis_key = f"{topic}:{sensor_id}"  # Create a Redis key using the topic and sensor_id
+    redis_client.lpush(redis_key, json.dumps(payload, default=str))
+    redis_client.ltrim(redis_key, 0, 9)
 
-def store_in_mongodb(payload):
+def store_in_mongodb(payload, topic):
     client = pymongo.MongoClient("mongodb://mongodb-container:27017/")  # Use the container name of MongoDB
     db = client["sensor_db"]
-    collection = db["sensor_readings"]
+    # Use the topic as the collection name
+    collection = db[topic]  
     collection.insert_one(payload)
 
 def on_message(client, userdata, msg):
     payload = json.loads(msg.payload)
-    store_in_mongodb(payload)
-    update_redis(payload)
+    topic = msg.topic  # Get the topic from the MQTT message
+    topic = topic.replace("/", "_")
+    store_in_mongodb(payload, topic)
+    update_redis(payload, topic)
 
 client.on_connect = on_connect
 client.on_message = on_message
@@ -31,5 +35,5 @@ client.on_message = on_message
 # Initialize Redis client
 redis_client = redis.Redis(host='redis-container', port=6379, db=0)
 
-client.connect("mqtt-broker-container", 1883, 60)  # Use the container name of the MQTT broker
+client.connect("mqtt-broker-container", 1883, 60)  # Use the IP address of the MQTT broker
 client.loop_forever()
